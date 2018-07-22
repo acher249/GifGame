8b4c8c15a8f7b9bb0a0045721bfa6e3ac0879057
Adam <jchernick2010@gmail.com>
2018-07-21 23:39:02 -0400

    input js
    input js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,23 @@
 window.onload = function() {
     //on start
-    
+    welcomeAlert();
 
 };
+
+$("#addButton").on("click", function(event) {
+    var newGifInput = $(".form-control").val();
+    console.log(newGifInput);
+});
+
+//Alerts******************
+ function welcomeAlert() {    
+    swal({
+        title: "Hope You Like Gifs",
+        icon: "success",
+        text: "because you are going to see a lot of gifs",
+        button: "Gif Time", 
+    })
+    .then(() => {
+        //Do somthing on click
+        });
+    }
